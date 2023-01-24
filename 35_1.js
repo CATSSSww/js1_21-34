1f8f96926056464171d806eef16a597e0e83c3b7
CATSSSww <117073206+CATSSSww@users.noreply.github.com>
2023-01-24 19:21:50 +0500

    Add files via upload

---
+++ 35_1.js
@@ -0,0 +1,50 @@
+"use strict"
+let num1 = 1;
+let num2 = 2;
+console.log('сумма: ' + num1 + num2); 
+
+let a = 1;
+let b = 2;
+console.log(a + b);
+
+let num = '123';
+let sum = num[0] + num[1] + num[2];
+console.log(sum);
+
+let num_1 = 123;
+console.log(num[0]);
+
+let a_1 = 1;
+console.log(--a);
+
+let num_2 = 123;
+console.log(num.length);
+
+let a_2 = 100;
+console.log(a_2*24*60*60);
+
+let num_3 = 12355;
+let str = String(num_3);
+console.log(str.length);
+
+let num_4 = 123;
+let str_1 = String(num_4);
+console.log(str_1[str_1.length -1]);
+
+let num_5 = 123;
+let str_2 = String(num_5);
+console.log(str_2[str_2.length -1]);
+
+let num_6 = 123;
+let str_3 = String(num_6);
+console.log(str_3[str_3.length -1]);
+
+let num_7 = '123';
+let num_8 = '456';
+let s_0 = Number(num_7) + String(num_8);
+console.log(s_0);
+
+let num_9 = 1;
+let num_10 = 2;
+let num_11 = 3;
+console.log(num_9 + num_10 + num_11);
---
+++ 37_1.js
@@ -0,0 +1,27 @@
+"use strict"
+/*let r = 20
+pi = 3.14
+s = pi * r**2
+console.log(s)*/
+
+// let i = 5
+// let s = i*i
+// console.log(s)
+
+// let u = 2 
+// let y = 6
+// let p = (u + y)*(u + y)
+// console.log(p)
+
+// let u = 3 
+// let y = 7
+// let s = (u + y) * 2
+// console.log(s)
+
+let tc = 20
+let F = tc * 1.8 + 32
+console.log(F)
+
+let tf = 68
+let C = (tf - 32) / 1.8
+console.log(C)
